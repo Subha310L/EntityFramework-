a7a00ffb0ebc62b90990111200b351536159f6d4
Subha Lakshmi Lakshmanaperumal <subhalakshmi.lakshmanaperumal@gyansys.com>
2022-12-20 17:06:21 +0530

    EntityFramework

---
+++ Controllers/ProductDescriptionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EntityFramework.Models;
+
+namespace EntityFramework.Controllers
+{
+    public class ProductDescriptionsController : Controller
+    {
+        private AdventureWorksLT2019Entities1 db = new AdventureWorksLT2019Entities1();
+
+        // GET: ProductDescriptions
+        public ActionResult Index()
+        {
+            return View(db.ProductDescriptions.ToList());
+        }
+
+        // GET: ProductDescriptions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductDescription productDescription = db.ProductDescriptions.Find(id);
+            if (productDescription == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productDescription);
+        }
+
+        // GET: ProductDescriptions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductDescriptions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductDescriptionID,Description,rowguid,ModifiedDate")] ProductDescription productDescription)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductDescriptions.Add(productDescription);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productDescription);
+        }
+
+        // GET: ProductDescriptions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductDescription productDescription = db.ProductDescriptions.Find(id);
+            if (productDescription == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productDescription);
+        }
+
+        // POST: ProductDescriptions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductDescriptionID,Description,rowguid,ModifiedDate")] ProductDescription productDescription)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productDescription).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productDescription);
+        }
+
+        // GET: ProductDescriptions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductDescription productDescription = db.ProductDescriptions.Find(id);
+            if (productDescription == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productDescription);
+        }
+
+        // POST: ProductDescriptions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductDescription productDescription = db.ProductDescriptions.Find(id);
+            db.ProductDescriptions.Remove(productDescription);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/ProductModelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EntityFramework.Models;
+
+namespace EntityFramework.Controllers
+{
+    public class ProductModelsController : Controller
+    {
+        private AdventureWorksLT2019Entities1 db = new AdventureWorksLT2019Entities1();
+
+        // GET: ProductModels
+        public ActionResult Index()
+        {
+            return View(db.ProductModels.ToList());
+        }
+
+        // GET: ProductModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductModel productModel = db.ProductModels.Find(id);
+            if (productModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productModel);
+        }
+
+        // GET: ProductModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductModelID,Name,CatalogDescription,rowguid,ModifiedDate")] ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductModels.Add(productModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productModel);
+        }
+
+        // GET: ProductModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductModel productModel = db.ProductModels.Find(id);
+            if (productModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productModel);
+        }
+
+        // POST: ProductModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductModelID,Name,CatalogDescription,rowguid,ModifiedDate")] ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productModel);
+        }
+
+        // GET: ProductModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductModel productModel = db.ProductModels.Find(id);
+            if (productModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productModel);
+        }
+
+        // POST: ProductModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductModel productModel = db.ProductModels.Find(id);
+            db.ProductModels.Remove(productModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
